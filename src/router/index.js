00678e65ecacc8005aae0f755c41fc5784a90e92
XiXi1103 <1239174672@qq.com>
2020-08-10 16:10:46 +0800

    整合markdown

--- src/router/index.js
+++ src/router/index.js
@@ -4,26 +4,20 @@ import Home from '../views/Home.vue'
 import Login from "../components/Login";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import Markdown from "../components/Markdown";
 
+
+Vue.use(mavonEditor)
 Vue.use(VueRouter)
 Vue.use(ElementUI);
 
   const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
+    {path: '/', name: 'Home', component: Home},
+    {path: '/about', name: 'About', component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')},
+    {path: '/login', name: 'Login', component: Login},
+    {path: '/mk', name: 'Markdown', component: Markdown},
 ]
 
 const router = new VueRouter({
