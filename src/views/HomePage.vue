ccbe96ac73d4f1d82a53a69cd195390d6d98c1d1
Aaron-LHR <lianhaoran999@gmail.com>
2020-08-11 23:05:19 +0800

    显示最近文档

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -63,34 +63,27 @@
                 <el-container>
                     <el-main>
                         <div id="doc">
+                            <el-row :gutter="12">
+                                <el-col :span="6">
+                                    <el-card shadow="hover" @click.native="GotoMarkDown" style="font-size: 20px; font-weight: bold; height: 210px;">
+                                        新建<i class="el-icon-plus"></i>
+<!--                                        <el-button type="info" @click="GotoMarkDown" style="width: 100%; height: 100%; background-color: white"><i class="el-icon-plus" style="size: auto"></i>新建</el-button>-->
+                                    </el-card>
+                                </el-col>
+                                <el-col :span="6" v-for="Page in pageList" :key="Page.title">
+                                    <el-card shadow="hover" @click.native="viewmk(0)" style="font-size: 20px; font-weight: bold; height: 210px;">
+<!--                                        <el-button icon="el-icon-more" circle style="float: right"></el-button><br>-->
+                                        <el-dropdown trigger="hover" style="float: right;">
+                                            <i class="el-icon-more"></i>
+<!--                                            <el-button icon="el-icon-more" circle style="float: right" type="info"></el-button>-->
+                                            <el-dropdown-menu slot="dropdown" style="float: right">
+                                                <el-dropdown-item @click.native="editmk(0)">修改文章</el-dropdown-item>
+                                                <el-dropdown-item @click.native="editmk(0)">分享</el-dropdown-item>
+                                                <el-dropdown-item @click.native="editmk(0)" style="color:red">删除</el-dropdown-item>
+                                            </el-dropdown-menu>
+                                        </el-dropdown>
 
-<!--&lt;!&ndash;                            <el-button type="info" style="float:left" @click="CreateTeam">创建团队</el-button>&ndash;&gt;-->
-
-                            <el-row :gutter="12" style="margin-bottom: 50px">
-                                <el-col :span="8">
-                                    <el-card shadow="hover">
-                                        <el-button type="info" @click="GotoMarkDown">新建</el-button>
-                                    </el-card>
-                                </el-col>
-                                <el-col :span="8">
-                                    <el-card shadow="hover">
-                                        文章在这里显示<br>
-                                        <el-button type="info" @click="viewmk(0)">查看文章</el-button>
-                                        <el-button type="info" @click="editmk(0)">修改文章</el-button>
-                                    </el-card>
-                                </el-col>
-                                <el-col :span="8">
-                                    <el-card shadow="hover">
-                                        文章在这里显示<br>
-                                        <el-button type="info" @click="viewmk(0)">查看文章</el-button>
-                                        <el-button type="info" @click="editmk(0)">修改文章</el-button>
-                                    </el-card>
-                                </el-col>
-                                <el-col :span="8">
-                                    <el-card shadow="hover">
-                                        文章在这里显示<br>
-                                        <el-button type="info" @click="viewmk(0)">查看文章</el-button>
-                                        <el-button type="info" @click="editmk(0)">修改文章</el-button>
+                                        <span style="text-align: center; display: block">{{Page.title}}</span>
                                     </el-card>
                                 </el-col>
                             </el-row>
@@ -111,13 +104,29 @@
 
 <script>
     import Topbar from "../components/Topbar";
+
     export default {
+        data() {
+            return {
+                pageList : []
+            }
+        },
+        created() {
+            this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/getRecentPage",{
+                params:{
+                    userID:sessionStorage.getItem("userId"),
+                }
+            }).then(res=>{
+                this.pageList = res.data.PageList;
+                console.log(this.pageList)
+            });
+        },
         methods: {
             GotoMarkDown:function(){
                 this.$router.push({path:'/markdown'});
             },
             viewmk(DocID){
-                this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/viewmk",{
+                this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/viewDoc",{
                     params:{
                         userID:sessionStorage.getItem("userId"),
                         docID:DocID
@@ -127,7 +136,7 @@
                 })
             },
             editmk(DocID){
-                this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/editmk",{
+                this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/editDoc",{
                     params:{
                         userID:sessionStorage.getItem("userId"),
                         docID:DocID
@@ -142,6 +151,16 @@
                     })
                 })
             },
+            // getRecentPage(){
+            //     this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/editDoc",{
+            //         params:{
+            //             userID:sessionStorage.getItem("userId"),
+            //             pageCategory:0
+            //         }
+            //     }).then(res=>{
+            //
+            //     })
+            // },
             handleOpen(key, keyPath) {
                 console.log(key, keyPath);
             },
@@ -187,5 +206,18 @@
 
     .el-container:nth-child(7) .el-aside {
         line-height: 320px;
+    }
+    .el-dropdown-link {
+        cursor: pointer;
+        color: #409EFF;
+    }
+    .el-icon-arrow-down {
+        font-size: 12px;
+    }
+    .demonstration {
+        display: block;
+        color: #8492a6;
+        font-size: 14px;
+        margin-bottom: 20px;
     }
 </style>
