bfb67bdf77bab9f4496bc7283281bed29d6c6312
wangsong3 <729045633@qq.com>
2020-08-12 15:18:53 +0800

    完善个人信息，新增回收站

--- src/views/PersonalInfo.vue
+++ src/views/PersonalInfo.vue
@@ -1,59 +1,105 @@
 <template>
-<div>
-  <el-button icon="el-icon-arrow-left" @click="back"> 返回主页</el-button>
-  <el-form  class="Info-container" label-position="left"
-           label-width="90px" v-loading="loading">
-    <h3 class="title">个人信息</h3>
-    <el-form-item label="用户名：">
-      <el-input type="text" v-model="user.username" auto-complete="off" disabled></el-input>
-    </el-form-item>
-    <el-form-item label="密码：">
-      <el-input type="password" v-model="user.passwd" auto-complete="off" :disabled="ischange"></el-input>
-    </el-form-item>    
-    <el-form-item label="邮箱：">
-      <el-input type="text" v-model="user.email" auto-complete="off" :disabled="ischange"></el-input>
-    </el-form-item>
-    <el-form-item label="电话：">
-      <el-input type="text" v-model="user.phone_num" auto-complete="off" :disabled="ischange"></el-input>
-    </el-form-item>
-    <el-form-item label="创建日期：">
-      <el-input type="text" v-model="user.create_time" auto-complete="off" disabled></el-input>
-    </el-form-item>
-    <br>
-    <el-form-item style="width: 75%">
-      <el-button type="primary" @click="changeinfo" style="width: 50%">编辑</el-button>
-    </el-form-item>
-    <el-form-item style="width: 75%">
-      <el-button @click="preserve" style="width: 50%">保存</el-button>
-    </el-form-item>
-  </el-form>
-</div>
+  <div>
+    <el-container>
+      <el-header>
+        <Topbar></Topbar>
+      </el-header>
+    <el-container>
+      <el-aside width="15%">
+          <el-row class="tac">
+              <el-col>
+                  <h5>默认颜色</h5>
+                  <el-menu
+                          router :default-active="$route.path"
+                          class="el-menu-vertical-demo"
+                          @open="handleOpen"
+                          @close="handleClose">
+                      <el-menu-item >
+                          <i class="el-icon-document"></i>
+                          <span slot="title">我的信息</span>
+                      </el-menu-item>
+                  </el-menu>
+              </el-col>
+          </el-row>
+      </el-aside>  
+    <el-container>
+      <el-main>
+      <el-form  class="Info-container" label-position="left"
+          label-width="90px" v-loading="loading">
+        <h2 class="title">个人信息</h2>
+        <el-form-item label="用户名：">
+          <el-input type="text" v-model="user.username" auto-complete="off" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="密码：">
+          <el-input type="password" v-model="user.passwd" auto-complete="off" :disabled="ischange"></el-input>
+        </el-form-item>    
+        <el-form-item label="邮箱：">
+          <el-input type="text" v-model="user.email" auto-complete="off" :disabled="ischange"></el-input>
+        </el-form-item>
+        <el-form-item label="电话：">
+          <el-input type="text" v-model="user.phone_num" auto-complete="off" :disabled="ischange"></el-input>
+        </el-form-item>
+        <el-form-item label="创建日期：">
+          <el-input type="text" v-model="user.create_time" auto-complete="off" disabled></el-input>
+        </el-form-item>
+        <el-form-item style="width: 75%">
+          <el-button type="primary" @click="changeinfo" style="width: 50%">编辑</el-button>
+        </el-form-item>
+        <el-form-item style="width: 75%">
+          <el-button @click="preserve" style="width: 50%">保存</el-button>
+        </el-form-item>
+      </el-form>
+      </el-main>
+    </el-container>
+    </el-container>
+    </el-container>
+  </div>
 </template>
-<script>
-  export default{
-    data(){
-      return {
-        checked: true,
-        ischange: 1,
-        user: {
-          username: "",
-          passwd: "",
-          email:"",
-          phone_num:"", 
-          create_time:"",
-        },
-        loading: false,
 
-      }
-    },
-    methods: {
+<script>
+  import Topbar from "../components/Topbar";
+  export default {
+      name: "RecycleBin",
+      components:{
+          Topbar,
+      },
+      data(){
+        return {
+          checked: true,
+          ischange: 1,
+          user: {
+            username: "",
+            passwd: "",
+            email:"",
+            phone_num:"", 
+            create_time:"",
+          },
+          loading: false,
+
+        }
+      },
+      methods: {
         changeinfo:function(){
             this.ischange=0;
         },
         preserve:function(){
             this.ischange=1;
-            this.$http.post("http://rap2.taobao.org:38080/app/mock/262266/personalinfo",this.user)
-            .then(res=>{console.log(res)})
+            this.$http.post("http://rap2.taobao.org:38080/app/mock/262266/changePerInfo",{
+              param:{
+                userID: sessionStorage.getItem("userId"),
+                passwd: this.passwd,
+                email: this.email,
+                phone_num: this.phone_num
+              }
+            })
+            .then(res=>{
+              if(res.data.success){
+                alert("保存成功");
+              }
+              else{
+                alert("修改失败");
+              }
+            })
         },
         back:function(){
           if(this.ischange == 0){
@@ -69,25 +115,27 @@
           }
           else{
             this.$router.push('/homepage');
-          }
-          
+          }      
+      }
+  },
+  created() {
+      this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/personalInfo",{
+        param:{
+          userid:sessionStorage.getItem("userId"),
         }
-    },
-    created() {
-        this.user.username = sessionStorage.getItem("username");
-        this.user.passwd = sessionStorage.getItem("passwd");
-        this.user.email = sessionStorage.getItem("email");
-        this.user.phone_num = sessionStorage.getItem("phone_num");
-        this.user.create_time = sessionStorage.getItem("create_time");
-    }
+      }).then(res=>{
+        this.user = res.data;
+      })
+  }
 }
 </script>
+
 <style>
   .Info-container {
     border-radius: 15px;
     background-clip: padding-box;
-    margin: 180px auto;
-    width: 400px;
+    margin: 90px auto;
+    width: 450px;
     padding: 35px 35px 15px 35px;
     background: #fff;
     border: 1px solid #eaeaea;
@@ -95,9 +143,54 @@
   }
 
   .title {
-    margin: 0px auto 40px auto;
+    margin: 0px auto 5px auto;
     text-align: center;
     color: #505458;
   }
+    .el-header, .el-footer {
+        background-color: white;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
 
+    .el-aside {
+        background-color: white;
+        color: #333;
+        text-align: center;
+        /*line-height: 200px;*/
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
+    }
+
+    body > .el-container {
+        margin-bottom: 40px;
+    }
+
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
+    }
+
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
+    }
+    .el-dropdown-link {
+        cursor: pointer;
+        color: #409EFF;
+    }
+    .el-icon-arrow-down {
+        font-size: 12px;
+    }
+    .demonstration {
+        display: block;
+        color: #8492a6;
+        font-size: 14px;
+        margin-bottom: 20px;
+    }
 </style>
--- src/views/RecycleBin.vue
+++ src/views/RecycleBin.vue
@@ -1,8 +1,36 @@
 <template>
     <div>
-        <Topbar></Topbar>
-        <Asidebar></Asidebar>
-        <span>回收站</span>
+        <el-container>
+            <el-header>
+              <Topbar></Topbar>
+            </el-header>
+            <el-container>
+            <Asidebar></Asidebar>
+            <el-container>
+                <el-main>
+                    <div id="doc">
+                        <el-row :gutter="12">
+                            <el-col :span="6" v-for="(item,index) in delList" :key="item.title">
+                                <el-card shadow="hover" @click.native="viewmk(0)" style="font-size: 20px; font-weight: bold; height: 210px;">
+<!--                                        <el-button icon="el-icon-more" circle style="float: right"></el-button><br>-->
+                                    <el-dropdown trigger="hover" style="float: right;">
+                                        <i class="el-icon-more"></i>
+<!--                                            <el-button icon="el-icon-more" circle style="float: right" type="info"></el-button>-->
+                                        <el-dropdown-menu slot="dropdown" style="float: right">
+                                            <el-dropdown-item @click.native="recover(index)">恢复</el-dropdown-item>
+                                            <el-dropdown-item @click.native="del(index)" style="color:red">删除</el-dropdown-item>
+                                        </el-dropdown-menu>
+                                    </el-dropdown>
+
+                                    <span style="text-align: center; display: block">{{item.title}}</span>
+                                </el-card>
+                            </el-col>
+                        </el-row>
+                    </div>
+                </el-main>
+            </el-container>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
@@ -14,10 +42,128 @@
         components:{
             Topbar,
             Asidebar
+        },
+        data(){
+            return{
+                delList: [],
+                count: 0
+            }
+        },
+        created() {
+            this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/getDelDoc",{
+                params:{
+                    userID:sessionStorage.getItem("userId"),
+                }
+            }).then(res=>{
+                this.delList = res.data.DelList;
+                console.log(this.DelList)
+            });
+        },
+        methods:{
+            load(){
+                this.count +=2
+            },
+            viewmk(DocID){
+                this.$http.get("http://rap2.taobao.org:38080/app/mock/262266/viewmk",{
+                    params:{
+                        userID:sessionStorage.getItem("userId"),
+                        docID:DocID
+                    }
+                }).then(res=>{
+                    if (res.data.success){
+                        this.$router.push({
+                            path: '/ShowDoc',
+                            query:{
+                                content: res.data.content,
+                            }
+                        })
+                    }
+                    else {
+                        alert(res.data.msg);
+                    }
+                })
+            },
+            recover:function(DocID){
+                this.$http.post("http://rap2.taobao.org:38080/app/mock/262266/recoverDoc",{
+                    params:{
+                        userID:sessionStorage.getItem("userId"),
+                        docID:DocID
+                    }
+                }).then(res=>{
+                    if (res.data.success){
+                        alert("恢复成功");
+                    }
+                    else {
+                        alert("恢复失败");
+                    }
+                })
+                this.delList.splice(DocID,1)
+            },
+            del:function(DocID){
+                this.$http.post("http://rap2.taobao.org:38080/app/mock/262266/delDoc",{
+                    params:{
+                        userID:sessionStorage.getItem("userId"),
+                        docID:DocID
+                    }
+                }).then(res=>{
+                    if (res.data.success){
+                        alert("删除成功");
+                    }
+                    else {
+                        alert("删除失败");
+                    }
+                })
+                this.delList.splice(DocID,1)
+            }
         }
     }
 </script>
 
-<style scoped>
+<style>
+    .el-header, .el-footer {
+        background-color: white;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
 
+    .el-aside {
+        background-color: white;
+        color: #333;
+        text-align: center;
+        /*line-height: 200px;*/
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
+    }
+
+    body > .el-container {
+        margin-bottom: 40px;
+    }
+
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
+    }
+
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
+    }
+    .el-dropdown-link {
+        cursor: pointer;
+        color: #409EFF;
+    }
+    .el-icon-arrow-down {
+        font-size: 12px;
+    }
+    .demonstration {
+        display: block;
+        color: #8492a6;
+        font-size: 14px;
+        margin-bottom: 20px;
+    }
 </style>
