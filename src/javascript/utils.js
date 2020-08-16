9e3c8120c7c22d21c9399d643976eb02976fedf9
wangsong3 <63182005+wangsong3@users.noreply.github.com>
2020-08-16 15:13:52 +0800

    修改团队成员权限

--- src/javascript/utils.js
+++ src/javascript/utils.js
@@ -274,13 +274,29 @@ export default {
                     ID:DocID
                 }
             }).then(res=>{
-                if (res.data.success){
+                if (res.data.success==1){
                     alert("已添加为我的模板，快去编辑吧");
                 }
                 else alert(res.data.msg);
 
             })
         };
+        Vue.prototype.changePermission = function(id,permission){
+            this.$http.get(this.requestUrl+"/changePermission",{
+                params:{
+                    userID1:sessionStorage.getItem("userId"),
+                    groupID:sessionStorage.getItem("groupId"),
+                    permission:permission,
+                    userID2:id,
+                }
+            }).then(res=>{
+                console.log(res.data);
+                if(res.data.success){
+                    alert(res.data.msg);
+                }
+                else alert("权限不足");
+            })
+        }
         // Vue.prototype.delDoc = function (DocID, PageList){
         //     this.$http.post(this.requestUrl+"/delDoc",{
         //         params:{
