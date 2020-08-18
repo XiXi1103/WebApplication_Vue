9252e8baad937a15396811d5a80c4bc8d135e03e
UnYan <415212688@qq.com>
2020-08-18 19:08:17 +0800

    我想摸鱼

--- src/views/Mycreate.vue
+++ src/views/Mycreate.vue
@@ -10,7 +10,7 @@
                     <el-main>
                         <div class="block" style="line-height: normal">
                             <el-timeline>
-                                <el-timeline-item v-for="Pages in res.pageList" :key="Pages.date" timestamp=Pages.date placement="top">
+                                <el-timeline-item v-for="Pages in this.res.pageList" :key="Pages.date" timestamp=Pages.date placement="top">
                                     <el-row :gutter="14">
                                         <el-col :span="12" v-for="Page in Pages" :key="Page.id">
                                             <el-card shadow="hover" :body-style="{ padding: '0px' }" style="margin-bottom: 10px" @click.native="viewmk(Page.id)">
@@ -247,6 +247,7 @@
         created() {
             sessionStorage.setItem("type",1);
             this.getMyPage(this.res);
+            // console.log(this.res);
         },
         methods : {
             delDoc:function(docId){
