54036a74125261f458683ab9e7fcad2cb4bf0f91
Aaron-LHR <lianhaoran999@gmail.com>
2020-08-19 16:08:19 +0800

    样式

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,23 @@
 <template>
-  <el-form  class="login-container" label-position="left"
-           label-width="0px" v-loading="loading">
-    <h3 class="login_title">登录</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model="user.username" auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item prop="checkPass">
-      <el-input type="password" v-model="user.password" auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <br>
-    <el-form-item style="width: 100%">
-      <el-button type="primary" @click="submit" style="width: 50%">登录</el-button>
-    </el-form-item>
-    <el-button type="text" @click="gotoRegister">还没有账号?快来注册吧</el-button>
-<!--    <router-link :to="{path:'/register'}" >还没有账号?快去注册</router-link>-->
-  </el-form>
+<!--  <div :style="{backgroundImage:'url('+require('@/assets/backgroud1.jpg')+')'}" style="height: 100%;width: 100%">-->
+  <div id="full">
+    <el-form  class="login-container" label-position="left"
+              label-width="0px" v-loading="loading">
+      <h3 class="login_title">登录</h3>
+      <el-form-item prop="account">
+        <el-input type="text" v-model="user.username" auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item prop="checkPass">
+        <el-input type="password" v-model="user.password" auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <br>
+      <el-form-item style="width: 100%">
+        <el-button type="primary" @click="submit" style="width: 50%">登录</el-button>
+      </el-form-item>
+      <el-button type="text" @click="gotoRegister">还没有账号?快来注册吧</el-button>
+      <!--    <router-link :to="{path:'/register'}" >还没有账号?快去注册</router-link>-->
+    </el-form>
+  </div>
 </template>
 <script>
   export default{
@@ -82,5 +85,16 @@
   .login_remember {
     margin: 0px 0px 35px 0px;
     text-align: left;
+  }
+
+  body {
+    height: 0;
+  }
+  #full{
+    background:url("../assets/backgroud1.jpg");
+    width:100%;
+    height:100%;
+    position:fixed;
+    background-size:100% 100%;
   }
 </style>
